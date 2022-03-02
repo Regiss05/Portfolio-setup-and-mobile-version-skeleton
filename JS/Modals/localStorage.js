957a776a4cfed3a2487d93b22c7b16304d02ca14
regiss05 <regiss.steven@gmail.com>
2022-03-02 17:22:26 +0200

    local storage of data

---
+++ JS/Modals/localStorage.js
@@ -0,0 +1,11 @@
+const myObject = {
+  name: 'Keeping track of hundreds of components website',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+  featured: 'pupupp',
+  image: 'image',
+  technologies: 'html, bootstrap, Ruby on rails',
+  'live Version': 'https://regiss05.github.io/Portfolio-setup-and-mobile-version-skeleton/',
+  source: 'https://github.com/Regiss05/Portfolio-setup-and-mobile-version-skeleton',
+};
+
+window.localStorage.setItem('myObject', JSON.stringify(myObject));
