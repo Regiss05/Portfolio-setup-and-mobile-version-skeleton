5f3ef40abe5c41fe141307c2bb32aa80ca5ae772
regiss05 <regiss.steven@gmail.com>
2022-03-03 11:39:14 +0200

    button close fix

--- JS/Modals/localStorage.js
+++ JS/Modals/localStorage.js
@@ -6,8 +6,8 @@ const popupps = [
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
     featured_image: '',
     technologies: ['html', 'Bootstrap', 'Ruby on rails'],
-    link_version: 'See Live',
-    link_source: 'See Source',
+    linkVersion: 'See Live',
+    linkSource: 'See Source',
   },
 ];
 
@@ -20,11 +20,25 @@ const createPopuppElement = ({
   linkSource,
 }) => {
   const popuppDiv = document.createElement('div');
-  const popuppName = document.createElement('h2');
+
+  const headerPopupp = document.createElement('div');
+  headerPopupp.classList.add('modal-header');
+
+  const popuppName = document.createElement('h3');
+
+  const popuppDivImgPar = document.createElement('div');
+  popuppDivImgPar.classList.add('modal-body');
+
   const popuppDescription = document.createElement('p');
+  popuppDescription.classList.add('paragraph-popupp');
+
   const popuppImage = document.createElement('img');
   popuppImage.src = './Images/Snapshoot.svg';
+
   const popuppTech = document.createElement('li');
+
+  const divButtons = document.createElement('div');
+  divButtons.classList.add('buttons-popp');
   const popuppVersion = document.createElement('button');
   const popuppSource = document.createElement('button');
 
@@ -35,13 +49,28 @@ const createPopuppElement = ({
   popuppVersion.innerText = `${linkVersion}`;
   popuppSource.innerText = `${linkSource}`;
 
-  popuppDiv.append(
+  headerPopupp.append(
     popuppName,
-    popuppTech,
+  );
+
+  popuppDivImgPar.append(
     popuppImage,
     popuppDescription,
+  );
+
+  divButtons.append(
     popuppVersion,
     popuppSource,
+  );
+
+  popuppDescription.append(
+    divButtons,
+  );
+
+  popuppDiv.append(
+    headerPopupp,
+    popuppTech,
+    popuppDivImgPar,
   );
 
   poppuyContainer.appendChild(popuppDiv);
